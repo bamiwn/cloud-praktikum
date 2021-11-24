366a2dc094749aa0f8d1eb7f2a3d96eb7409932b
bamiwn <bambang.awan-2019@vokasi.unair.ac.id>
2021-11-24 11:02:33 +0700

    modif app py, ganti db jadi mydb gajadi database.sqlite

--- app.py
+++ app.py
@@ -1,18 +1,39 @@
 from flask import Flask
-app = Flask(__name__)
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
-
-
 from flask import jsonify
 import json
 import sqlite3
-@app.route("/api/v1/info")
-def home_index():
-    conn = sqlite3.connect('database.sqlite')
+
+app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
+
+@app.route('/api/v1/users', methods=['GET'])
+def get_users():
+    return list_users()
+
+def list_users():
+    conn = sqlite3.connect('mydb.db')
     print ("Opened database successfully");
     api_list=[]
-    cursor = conn.execute("SELECT buildtime, version, methods, links from apirelease")
+    cursor = conn.execute("SELECT username, full_name, emailid, password, id from users")
+
+    for row in cursor:
+        a_dict = {}
+        a_dict['username'] = row[0]
+        a_dict['name'] = row[1]
+        a_dict['emailid'] = row[2]
+        a_dict['password'] = row[3]
+        a_dict['id'] = row[4]
+        api_list.append(a_dict)
+        
+    conn.close()
+    return jsonify({'user_list': api_list})
+
+@app.route("/api/v1/info")
+def home_index():
+    conn = sqlite3.connect('mydb.db')
+    print ("Opened database successfully");
+    api_list=[]
+    cursor = conn.execute("SELECT buildtime, version, links, methods from apirelease")
     for row in cursor:
         a_dict = {}
         a_dict['version'] = row[0]
@@ -24,25 +45,8 @@ def home_index():
     conn.close()
     return jsonify({'api_version': api_list}), 200
 
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
-@app.route('/api/v1/users', methods=['GET'])
-def get_users():
-    return list_users()
 
-def list_users():
-    conn = sqlite3.connect('database.sqlite')
-    print ("Opened database successfully");
-    api_list=[]
-    cursor = conn.execute("SELECT username, full_name, email, password, id from users")
 
-    for row in cursor:
-        a_dict = {}
-        a_dict['username'] = row[0]
-        a_dict['name'] = row[1]
-        a_dict['email'] = row[2]
-        a_dict['password'] = row[3]
-        a_dict['id'] = row[4]
-        api_list.append(a_dict)
-
-    conn.close()
-    return jsonify({'user_list': api_list})
