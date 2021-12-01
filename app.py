655f94bb834cfa3ed62c1ceac1811e46fea19839
bamiwn <bambang.awan-2019@vokasi.unair.ac.id>
2021-12-01 10:43:08 +0700

    nambahin fitur CRUD users yang sudah bisa

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 from flask import Flask
+from flask import abort
 from flask import jsonify
+from flask import make_response
+from flask import request
+from flask import url_for
+from flask import gmtime
+from time import strftime
 import json
 import sqlite3
 
@@ -28,6 +34,55 @@ def list_users():
     conn.close()
     return jsonify({'user_list': api_list})
 
+
+@app.route('/api/v1/users/<int:user_id>', methods=['GET'])
+def get_user(user_id):
+    return list_user(user_id)
+
+def list_user(user_id):
+    conn = sqlite3.connect('mydb.db')
+    print ("Opened database successfully");
+    api_list=[]
+    cursor=conn.cursor()
+    cursor.execute("SELECT * from users where id=?",(user_id,))
+    data = cursor.fetchall()
+    print (data)
+    if len(data) != 0 :
+        user = {}
+        user['username'] = data[0][0]
+        user['name'] = data[0][1]
+        user['email'] = data[0][2]
+        user['password'] = data[0][3]
+        user['id'] = data[0][4]
+    else:
+        abort(404)
+
+    conn.close()
+    return jsonify(user)
+
+
+def add_user(new_user):
+    conn = sqlite3.connect('mydb.db')
+    print ("Opened database successfully");
+    api_list=[]
+    cursor=conn.cursor()
+    cursor.execute("SELECT * from users where username=? or email=?",(new_user['username'],new_user['email']))
+    data = cursor.fetchall()
+    if len(data) != 0:
+        abort(409)
+    else:
+       cursor.execute("insert into users (username, email, password, full_name) values(?,?,?,?)",(new_user['username'],new_user['email'], new_user['password'], new_user['name']))
+       conn.commit()
+       return "Success"
+    conn.close()
+    return jsonify(a_dict)
+
+
+@app.errorhandler(404)
+def resource_not_found(error):
+    return make_response(jsonify({'error': 'Resource not found!'}), 404)
+
+
 @app.route("/api/v1/info")
 def home_index():
     conn = sqlite3.connect('mydb.db')
@@ -45,8 +100,24 @@ def home_index():
     conn.close()
     return jsonify({'api_version': api_list}), 200
 
+
+@app.route('/api/v1/users', methods=['POST'])
+def create_user():
+    if not request.json or not 'username' in request.json or not 'email' in request.json or not 'password' in request.json:
+        abort(400)
+    user = {
+        'username': request.json['username'],
+        'email': request.json['email'],
+        'name': request.json.get('name',""),
+        'password': request.json['password']
+    }
+    return jsonify({'status': add_user(user)}), 201
+
+@app.errorhandler(400)
+def invalid_request(error):
+    return make_response(jsonify({'error': 'Bad Request'}), 400)
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
-
 
 
